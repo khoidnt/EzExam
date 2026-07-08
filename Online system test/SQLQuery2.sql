19c8e51a0145492a9cf5a2b036b378c4258c88ee
Đinh Ngô Thái Khôi <khoidnt2789@ut.edu.vn>
2026-07-08 23:52:55 +0700

    Update new files

--- Online system test/SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-﻿-- Đưa hết các giá trị chữ về số 0 để không bị lỗi khi chuyển đổi
-UPDATE [dbo].[Users] SET [Role] = 0 WHERE ISNUMERIC([Role]) = 0 OR [Role] IS NULL;
-GO
-
--- Sau đó mới thực hiện chuyển kiểu
-ALTER TABLE [dbo].[Users] ALTER COLUMN [Role] INT;
-GO
--- Online system test/SQLQuery3.sql
+++
@@ -1,2 +0,0 @@
-﻿-- Cập nhật quyền Admin cho tài khoản của bạn
-UPDATE [dbo].[Users] SET Role = 1 WHERE Username = 'phat2122006';
